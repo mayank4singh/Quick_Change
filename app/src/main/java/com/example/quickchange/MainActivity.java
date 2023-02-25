fcee740f1a27803a8bb67a64291671443b420473
mayank4singh <mailatmausam@gmail.com>
2023-02-25 20:30:06 +0530

    first commit

---
+++ app/src/main/java/com/example/quickchange/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.quickchange;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity {
+    RelativeLayout layout;
+    RadioGroup radioGroup;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        layout =findViewById(R.id.layout);
+        radioGroup =findViewById(R.id.radio);
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int checked) {
+                switch (checked){
+                    case R.id.btnBlue:
+                        layout.setBackgroundColor(Color.parseColor("#3944F7"));
+                        break;
+                    case R.id.btnYellow:
+                        layout.setBackgroundColor(Color.parseColor("#F4BE2C"));
+                        break;
+                    case R.id.btnRed:
+                        layout.setBackgroundColor(Color.parseColor("#E21717"));
+                        break;
+
+                }
+
+            }
+        });
+
+
+
+
+
+    }
+}
